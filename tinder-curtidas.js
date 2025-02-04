44659b4137c5e00879dabd21d9e12672c8470a2e
Michael Rozendo <mikerozendo@gmail.com>
2025-02-04 20:58:45 -0300

    Update tinder-curtidas.js

--- tinder-curtidas.js
+++ tinder-curtidas.js
@@ -1,12 +1,23 @@
-var buttonLike = document.querySelectorAll('button')[28];
-
-if(buttonLike != null && buttonLike != undefined){
-
+var spans = [];
+spans.push(document.querySelectorAll('span.Hidden'));
+var spanHidden = spans[0];
+ 
+var spanCurtida = null;
+for(let i = 0; i < spanHidden.length; i++){
+	if(spanHidden[i].textContent == 'Curtir'){
+		console.log(spanHidden[i].textContent);
+		spanCurtida = spanHidden[i];	
+	}	
+}
+var buttonCurtida = null;
+if(spanCurtida != null && spanCurtida != undefined){
     var like = 0;
+	buttonCurtida = spanCurtida.closest('button');	
 	
 	Interval = setInterval(function() {
-		buttonLike.click();
+		buttonCurtida.click();
 		like++;
 		console.log('likes:' + like + '');
 	}, Math.floor(Math.random() * 4 + 2) * 1000);
+	
 }
